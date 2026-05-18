6e1dd6bd31fe27fd798376e76df5debdd4632cac
rahimshai <shaikhrahim882@gmail.com>
2026-05-18 22:40:51 +0530

    .

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'your_project_id',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'i22von0a',
   dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2024-05-04', // use current date (YYYY-MM-DD) to target the latest API version
